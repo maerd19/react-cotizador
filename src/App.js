4b1d6db7cf34251e13d07018177167c517423908
maerd19 <revolutions_333@hotmail.com>
2020-09-04 09:19:14 -0500

    Agregando Formulario con Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
-  return <Header titulo="Cotizador de seguros" />;
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de seguros" />
+
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
+  );
 }
 
 export default App;
